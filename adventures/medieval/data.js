59bdfdbb0a0a4306f037621bad4ddcfe955b8ad1
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-11-17 12:59:58 -0300

    feat(aventura medieval): adiciona assets visuais

--- adventures/medieval/data.js
+++ adventures/medieval/data.js
@@ -1,7 +1,7 @@
 const storyData = [
   {
     id: 1,
-    image: "img/castelo-entrada.jpg",
+    image: "img/id01-castelo-entrada-02.png",
     altText: "Um cavaleiro em armadura olhando para um grande castelo de pedra.",
     texto: "Você é um jovem cavaleiro chegando ao castelo do rei. O que você faz primeiro?",
     opcoes: [
@@ -13,7 +13,7 @@ const storyData = [
 
   {
     id: 2,
-    image: "img/salao-rei.jpg",
+    image: "img/id02-salao-rei-02.png",
     altText: "O rei sentado em seu trono, parecendo preocupado.",
     texto: "O rei pede que você recupere uma relíquia roubada pelo exército inimigo. Aceita a missão?",
     opcoes: [
@@ -24,7 +24,7 @@ const storyData = [
 
   {
     id: 3,
-    image: "img/quarto-secreto.jpg",
+    image: "img/id03-quarto-secreto.png",
     altText: "Um quarto empoeirado com um mapa antigo aberto sobre uma mesa.",
     texto: "Enquanto explora, você encontra um quarto secreto com um mapa antigo. O que faz?",
     opcoes: [
@@ -35,7 +35,7 @@ const storyData = [
 
   {
     id: 4,
-    image: "img/taverna.jpg",
+    image: "img/id04-taverna.png",
     altText: "Uma taverna medieval barulhenta, com um taverneiro limpando um copo.",
     texto: "Na taverna, você ouve rumores de uma traição dentro do castelo.",
     opcoes: [
@@ -46,7 +46,7 @@ const storyData = [
 
   {
     id: 5,
-    image: "img/cavaleiros-partindo.jpg",
+    image: "img/id05-cavaleiros-partindo-02.png",
     altText: "Cavaleiros cavalgando para fora do castelo ao pôr do sol.",
     texto: "Você parte em direção ao castelo inimigo com seus aliados.",
     opcoes: [
@@ -57,7 +57,7 @@ const storyData = [
 
   {
     id: 6,
-    image: "img/pátio-castelo.jpg",
+    image: "img/id06-patio-castelo.png",
     altText: "Soldados treinando no pátio do castelo, olhando para você com desdém.",
     texto: "Você fica no castelo e acaba sendo acusado de covardia.",
     final: "Sua honra foi manchada. Fim de sua jornada."
@@ -65,7 +65,7 @@ const storyData = [
 
   {
     id: 7,
-    image: "img/tesouro.jpg",
+    image: "img/id07-tesouro.png",
     altText: "Um baú aberto cheio de moedas de ouro e artefatos brilhantes.",
     texto: "Seguindo o mapa, você encontra um tesouro escondido e artefatos mágicos!",
     final: "Você se torna um herói rico e poderoso."
@@ -73,7 +73,7 @@ const storyData = [
 
   {
     id: 8,
-    image: "img/conselheiro-sombrio.jpg",
+    image: "img/id08-conselheiro-sombrio.png",
     altText: "Um conselheiro sussurrando no ouvido do rei enquanto olha para você.",
     texto: "Descobre que um conselheiro do rei planeja traição. Você:",
     opcoes: [
@@ -84,7 +84,7 @@ const storyData = [
 
   {
     id: 9,
-    image: "img/taverna-bagunca.jpg",
+    image: "img/id09-taverna-bagunca-02.png",
     altText: "Canecas de cerveja viradas em uma mesa de taverna bagunçada.",
     texto: "Você bebe demais e perde a chance de salvar o reino.",
     final: "O reino cai em ruínas sem sua ajuda.",
@@ -93,7 +93,7 @@ const storyData = [
 
   {
     id: 10,
-    image: "img/masmorra-inimiga.jpg",
+    image: "img/id10-masmorra-inimiga.png",
     altText: "Mãos agarrando as grades de uma cela de prisão escura.",
     texto: "No ataque frontal, você é capturado pelo inimigo.",
     final: "Prisioneiro do castelo inimigo, seu destino é incerto.",
@@ -102,7 +102,7 @@ const storyData = [
 
   {
     id: 11,
-    image: "img/vitoria-noturna.jpg",
+    image: "img/id11-vitoria-noturna.png",
     altText: "Cavaleiros comemorando com tochas em frente a um castelo à noite.",
     texto: "O ataque surpresa funciona e o reino é salvo!",
     final: "Você é celebrado como um herói.",
@@ -111,7 +111,7 @@ const storyData = [
   
   {
     id: 12,
-    image: "img/guerra-civil.jpg",
+    image: "img/id12-guerra-civil.png",
     altText: "Dois exércitos com bandeiras opostas se enfrentando em um campo de batalha.",
     texto: "Você desmascara o traidor, mas isso gera uma guerra civil.",
     final: "O reino se divide, e você deve escolher um lado.",
